dcbd234944b70fcaf42f06f5cde246d3c8ef193f
Charles Benca <zzazzbug@gmail.com>
2023-03-17 13:43:46 -0400

    terminé 5. commencé 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenueComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bienvenue/bienvenue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Inter-bienvenue',
+  templateUrl: './bienvenue.component.html',
+  styleUrls: ['./bienvenue.component.css']
+})
+export class BienvenueComponent {
+
+}
