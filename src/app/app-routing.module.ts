1b445dd071d8921df89e1e2e57383d7fe691699b
Charles Benca <zzazzbug@gmail.com>
2023-03-17 14:02:14 -0400

    terminé vidéo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {
+  BienvenueComponent
+} from "./bienvenue/bienvenue.component";
+
+const routes: Routes = [
+  {
+    path: "bienvenue",
+    component: BienvenueComponent
+  },
+  {
+    path: "",
+    redirectTo: "bienvenue",
+    pathMatch: "full"
+  },
+  { //si la route est inexistante, rediriger l'utilisateur
+    path: "**",
+    redirectTo: "bienvenue",
+    pathMatch: "full"
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
