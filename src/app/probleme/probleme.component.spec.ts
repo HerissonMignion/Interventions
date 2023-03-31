2692a809cab30ee85c957db68d832feec69d0ad1
Charles Benca <zzazzbug@gmail.com>
2023-03-31 13:16:38 -0400

    terminé 3 premiers tests unitaires sur la zone du prenom

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProblemeComponent } from './probleme.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ProblemeComponent', () => {
   let component: ProblemeComponent;
@@ -8,6 +9,9 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule
+      ],
       declarations: [ ProblemeComponent ]
     })
     .compileComponents();
@@ -20,4 +24,27 @@ describe('ProblemeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("zone prenom valide avec au moins 3 caractères", () => {
+    const zone = component.problemeForm.controls["prenom"];
+    zone.setValue("a".repeat(3));
+    expect(zone.valid).toBeTruthy();
+  });
+
+  it("zone prenom invalide avec 2 caractères ou moins", () => {
+    const zone = component.problemeForm.controls["prenom"];
+    zone.setValue("a".repeat(2));
+    expect(zone.valid).toBeFalsy();
+  });
+
+  it("zone prenom valide avec 200 caractères", () => {
+    const zone = component.problemeForm.controls["prenom"];
+    zone.setValue("a".repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
+
+
+
+
+
 });
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+ } from '@angular/forms';
 
 @Component({
   selector: 'Inter-probleme',
@@ -6,7 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./probleme.component.css']
 })
 export class ProblemeComponent {
+  problemeForm: FormGroup;
 
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit() {
+    this.problemeForm = this.fb.group({
+      prenom: ["", [Validators.minLength(3)]]
+    });
+  }
 
   save(): void {
     
